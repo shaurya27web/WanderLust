7225b1cc64e8069a26ee305c0734c72d335e42a0
shaurya27web <shauryatiwari0011@gmail.com>
2025-08-15 03:06:53 +0530

    New changes

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
-const Listing=require("./models/listing.js");
 const path=require("path");
 const methodOverride = require('method-override');
-app.use(methodOverride('_method'));
 const ejsMate=require("ejs-mate");
 
-app.use(express.urlencoded({extended:true}));//for parsing the data
+const Listing=require("./models/listing.js");
+const wrapAsync=require("./utils/wrapAsync.js");
+const ExpressError=require("./utils/ExpressError.js");
+
 app.engine("ejs",ejsMate);
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+app.use(express.urlencoded({extended:true}));//for parsing the data
 //for using the static file
 app.use(express.static(path.join(__dirname,"/public")));
+app.use(methodOverride('_method'));
 
 
 //Mongo DB connectivity
@@ -24,56 +30,48 @@ main().then(()=>{
 }).catch((err)=>{
     console.log(err)
 });
+
+
 //API creation
 app.get("/",(req,res)=>{
     res.send("Hi I am root");
 });
-//sample data
-// app.get("/testsampling",async(req,res)=>{
-//     let sampleListing=new Listing({
-//         title:"My Villa",
-//         description:"By the Beach",
-//         price: 1200,
-//         location:"Goa",
-//         country:"India",
-//         contact:"89324455",
-//     });
-
-//     await sampleListing.save();
-//     console.log("sample was saved");
-//     res.send("Succesfull testing");
-// })
 
 //listing request(Index Route)
-app.get("/listings",async(req,res)=>{
+app.get("/listings",wrapAsync(async(req,res)=>{
     const allListing=await Listing.find({});
     res.render("./listings/index.ejs",{allListing});
-});
+}));
 
 
 //New Route for list creation
-app.get("/listings/new",(req,res)=>{
-    res.render("listings/new.ejs")
-});
+app.get("/listings/new",wrapAsync(async(req,res)=>{
+    res.render("listings/new.ejs", { listing: { image: { url: "" } } });
+}));
+
 
 //Post Route for adding new list
-app.post("/listings",async(req,res)=>{
-    const newListing=new Listing(req.body.listing);
+app.post("/listings",wrapAsync(async(req,res,next)=>{
+    if(!req.body.listing){
+        throw new ExpressError(400,"Send valid data for listing");
+    }
+ const newListing=new Listing(req.body.listing);
     console.log(newListing);
     await newListing.save();
     res.redirect("/listings");
 
-});
+}))
+;
 
 //Edit Route
-app.get("/listings/:id/edit",async (req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
  let {id}=req.params;
     const listing=await Listing.findById(id);
     res.render("listings/edit.ejs",{listing});
-});
+}));
 
 //Update Route
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id",wrapAsync(async(req, res) => {
   const { id } = req.params;
   const { listing } = req.body;
 
@@ -89,29 +87,39 @@ app.put("/listings/:id", async (req, res) => {
   });
 
   res.redirect("/listings");
-});
+}));
 
 
 //Deletion of listing
-app.delete("/listings/:id",async(req,res)=>{
+app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     const deletedListing=await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
-});
+}));
 
 //Show Route
-app.get("/listings/:id",async(req,res)=>{
+app.get("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
-})
+}));
+
+// //all errors
+// app.all("*",(req,res,next) => {
+//     next(new ExpressError(404,"Page not found"));
+// });
+
+
+//Error handelling middleware
+app.use((err,req,res,next)=>{
+    let {statusCode=500,message="Something went Wrong!"}=err;
+    res.status(statusCode).send(message);
+    res.render("listings/error.ejs");
+});
+
 
 //creation of Route
 app.listen(8080,()=>{
     console.log("Server is listening to port 8080");
 });
-
-//EJS templating
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
 
